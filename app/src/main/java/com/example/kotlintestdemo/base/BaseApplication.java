f05b5f6878b2999ffc8d560027ca641fdd6d7629
wubabalubab <1277287760@qq.com>
2022-05-26 00:56:32 +0800

    gradle 版本修改

--- app/src/main/java/com/example/kotlintestdemo/base/BaseApplication.java
+++ app/src/main/java/com/example/kotlintestdemo/base/BaseApplication.java
@@ -5,10 +5,6 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
-import com.tencent.smtt.sdk.QbSdk;
-import com.tencent.smtt.sdk.TbsDownloader;
-import com.tencent.smtt.sdk.TbsListener;
-
 public class BaseApplication extends Application {
     public static BaseApplication Myapp;
     private static final String TAG = "BaseApplication";
@@ -17,33 +13,6 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Myapp=this;
-        QbSdk.setDownloadWithoutWifi(true);
-        QbSdk.PreInitCallback cb=new QbSdk.PreInitCallback() {
-            @Override
-            public void onCoreInitFinished() {
-            }
-            @Override
-            public void onViewInitFinished(boolean b) {
-                Log.e(TAG, "内核加载结果: "+b );
-            }
-        };
-        QbSdk.setTbsListener(new TbsListener() {
-            @Override
-            public void onDownloadFinish(int i) {
-            }
-            @Override
-            public void onInstallFinish(int i) {
-                Log.e(TAG, "onInstallFinish: 安装下载完成" );
-            }
-            @Override
-            public void onDownloadProgress(int i) {
-            }
-        });
-        QbSdk.initX5Environment(getApplicationContext(), cb);
-        boolean needdownload= TbsDownloader.needDownload(this, TbsDownloader.DOWNLOAD_OVERSEA_TBS);
-        if (needdownload) {
-            TbsDownloader.startDownload(this);
-        }
 
         ActivityManager activityManager= (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         Log.e(TAG, "onCreate: "+activityManager.getMemoryClass());
--- app/src/main/java/com/example/kotlintestdemo/mvp/view/activity/ResoursActivity.java
+++ app/src/main/java/com/example/kotlintestdemo/mvp/view/activity/ResoursActivity.java
@@ -2,70 +2,25 @@ package com.example.kotlintestdemo.mvp.view.activity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.kotlintestdemo.R;
 import com.example.kotlintestdemo.base.BaseActivity;
 import com.example.kotlintestdemo.util.MyConstant;
-import com.tencent.smtt.export.external.interfaces.WebResourceRequest;
-import com.tencent.smtt.export.external.interfaces.WebResourceResponse;
-import com.tencent.smtt.sdk.WebSettings;
-import com.tencent.smtt.sdk.WebView;
-import com.tencent.smtt.sdk.WebViewClient;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public class ResoursActivity extends BaseActivity {
 
-    @BindView(R.id.web_actresours)
-    WebView webView;
     @BindView(R.id.im_actresours)
     ImageView imBack;
     @BindView(R.id.im_actresours_collection)
     ImageView imCollection;
 
     private String url = "";
-    private WebViewClient client = new WebViewClient() {
-        public boolean shouldorrerUrl(WebView webView, String url) {
-            webView.loadUrl(url);
-            return true;
-        }
 
-        public void onResponseRequest(WebView webView, com.tencent.smtt.export.external.interfaces.HttpAuthHandler httpAuthHandler,
-                                      String host, String realm) {
-            boolean flag = httpAuthHandler.useHttpAuthUsernamePassword();
-        }
-
-        @Override
-        public void onPageStarted(WebView webView, String s, Bitmap bitmap) {
-            super.onPageStarted(webView, s, bitmap);
-        }
-
-        @Override
-        public void onPageFinished(WebView webView, String s) {
-            super.onPageFinished(webView, s);
-        }
-
-        @Override
-        public void onReceivedError(WebView webView, int i, String s, String s1) {
-            super.onReceivedError(webView, i, s, s1);
-        }
-
-        // TODO: 21-4-12 url intercepter
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView webView, String s) {
-            return super.shouldOverrideUrlLoading(webView, s);
-        }
-
-        @Override
-        public void onReceivedHttpError(WebView webView, WebResourceRequest webResourceRequest, WebResourceResponse webResourceResponse) {
-            super.onReceivedHttpError(webView, webResourceRequest, webResourceResponse);
-        }
-    };
 
     @OnClick({R.id.im_actresours_collection, R.id.im_actresours})
     public void OnClick(View view) {
@@ -88,9 +43,7 @@ public class ResoursActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (webView != null) {
-            webView.destroy();
-        }
+
     }
 
     @Override
@@ -107,20 +60,6 @@ public class ResoursActivity extends BaseActivity {
         if (intent != null) {
             url = intent.getStringExtra(MyConstant.CONTENT_URL);
         }
-        if (url != null && !TextUtils.isEmpty(url)) {
-            webView.loadUrl(url);
-        }
-        //支持javascript  
-        webView.getSettings().setJavaScriptEnabled(true);
-        // 设置可以支持缩放
-        webView.getSettings().setSupportZoom(true);
-        // 设置出现缩放工具
-        webView.getSettings().setBuiltInZoomControls(true);
-        //扩大比例的缩放
-        webView.getSettings().setUseWideViewPort(true);
-        //自适应屏幕
-        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.setWebViewClient(client);
+
     }
 }
